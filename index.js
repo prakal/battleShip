c42ae427a21d48e90eb9e3a04020357ab23a39be
Prakal <pranav.89@gmail.com>
2015-08-29 00:13:05 -0600

    add a listener for clicked entry

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('clicked', function(data){
+  	console.log(data,'clicked');
+  })
 });
 
 http.listen(3000, function(){
