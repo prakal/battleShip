cb838c7788533298c49c924b5152af4dad6b9bc5
Prakal <pranav.89@gmail.com>
2015-08-29 02:48:29 -0600

    implement state, and write emit and on for sending stateData across server and client

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ app.get('/', function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+var state = {};
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('getState', function(){
+  	socket.emit('sendState',state);
+  });
   socket.on('clicked', function(data){
   	// console.log(data,'clicked');
-  	io.emit('clicked id', data);
+  	// write to state so that new clients get state of previous updates
+  	if (state[data]) {
+  		delete state[data];
+  	} else {
+  		state[data] = true;
+  	} 
+  	io.emit('clicked id', data, state);
   })
 });
 
